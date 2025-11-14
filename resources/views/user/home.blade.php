0a158aeeebc301ae70ebc2ae559e1427b1df89f4
Moreeh Ahmed <146959594+Moreeh97@users.noreply.github.com>
2025-11-14 16:10:24 +0300

    feat: creating home for user

---
+++ resources/views/user/home.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('title', 'الصفحة الرئيسية')
+
+@section('content')
+<div class="row">
+    <div class="col-12">
+        <div class="card">
+            <div class="card-header">
+                <h3>مرحباً بك في الصفحة الرئيسية</h3>
+            </div>
+            <div class="card-body">
+                <p>أنت مسجل دخول كمستخدم عادي</p>
+                <p>هذه الصفحة خاصة بالمستخدمين المسجلين</p>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
