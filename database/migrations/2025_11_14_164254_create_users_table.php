6d77254bdb74d6ef9d21c3d2bf9a1a9834d8353a
Moreeh Ahmed <146959594+Moreeh97@users.noreply.github.com>
2025-11-14 19:44:16 +0300

    feat: fixing user modle

--- database/migrations/2025_11_14_164254_create_users_table.php
+++ database/migrations/2025_11_14_164254_create_users_table.php
@@ -6,20 +6,21 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    public function up()
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->enum('role', ['user', 'admin'])->default('user');
-            $table->rememberToken();
             $table->timestamps();
         });
     }
 
-    public function down()
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
     {
         Schema::dropIfExists('users');
     }
